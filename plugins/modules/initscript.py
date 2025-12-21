57d0d6a6d47d22334b5fec07a0a496bd3730b44a
normalerweise <normalerweise@weisenburger.ch>
2025-12-21 23:39:38 +0100

    fix initscript

--- plugins/modules/initscript.py
+++ plugins/modules/initscript.py
@@ -4,7 +4,7 @@ __metaclass__ = type
 
 # Manage init/shutdown scripts.
 
-DOCUMENTATION = '''
+DOCUMENTATION = """
 ---
 module: initscript
 short_description: Manage init/shutdown scripts.
@@ -58,9 +58,9 @@ options:
     choices: [ absent, present ]
     default: present
 version_added: 1.10.0
-'''
+"""
 
-EXAMPLES = '''
+EXAMPLES = """
 - name: Run a command late once everything has started
   initscript:
     name: Start Foo
@@ -82,18 +82,19 @@ EXAMPLES = '''
     name: Remove old cruft
     name: Start Foo
     state: absent
-'''
+"""
 
 # XXX
-RETURN = '''
+RETURN = """
 script:
   description:
     - A data structure describing a newly-created init script.
     - Only returned when a script is created.
   type: int
-'''
+"""
 
 from ansible.module_utils.basic import AnsibleModule
+
 from ..module_utils.middleware import MiddleWare as MW
 
 
@@ -101,50 +102,54 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             # XXX
-            name=dict(type='str'),
-            cmd=dict(type='str', aliases=['command']),
-            path=dict(type='str'),
-            script=dict(type='str'),
-            when=dict(type='str',
-                      choices=['preinit', 'postinit', 'shutdown',
-                               'PREINIT', 'POSTINIT', 'SHUTDOWN']),
-            timeout=dict(type='int'),
-            state=dict(type='str', default='present',
-                       choices=['absent', 'present']),
+            name=dict(type="str"),
+            cmd=dict(type="str", aliases=["command"]),
+            path=dict(type="str"),
+            script=dict(type="str"),
+            when=dict(
+                type="str",
+                choices=[
+                    "preinit",
+                    "postinit",
+                    "shutdown",
+                    "PREINIT",
+                    "POSTINIT",
+                    "SHUTDOWN",
+                ],
             ),
+            timeout=dict(type="int"),
+            state=dict(type="str", default="present", choices=["absent", "present"]),
+        ),
         supports_check_mode=True,
         # The upshot of the 'required_if' and 'mutually_exclusive'
         # constraints is that if you want the script to exist, you
         # have to provide exactly one of 'cmd', 'path', or 'script'.
         # If you want it to be absent, you don't have to provide any
         # of them.
-        required_if=[
-            ('state', 'present', ('cmd', 'path', 'script'), True)
-        ],
-        mutually_exclusive=['cmd', 'path', 'script'],
+        required_if=[("state", "present", ("cmd", "path", "script"), True)],
+        mutually_exclusive=["cmd", "path", "script"],
     )
 
     result = dict(
         changed=False,
-        msg='',
-        debug='',
+        msg="",
+        debug="",
     )
 
     mw = MW.client()
 
     # Assign variables from properties, for convenience
-    name = module.params['name']
-    cmd = module.params['cmd']
-    path = module.params['path']
-    script = module.params['script']
-    when = module.params['when']
-    timeout = module.params['timeout']
-    state = module.params['state']
+    name = module.params["name"]
+    cmd = module.params["cmd"]
+    path = module.params["path"]
+    script = module.params["script"]
+    when = module.params["when"]
+    timeout = module.params["timeout"]
+    state = module.params["state"]
 
     # Look up the script.
     try:
-        script_info = mw.call("initshutdownscript.query",
-                              [["comment", "=", name]])
+        script_info = mw.call("initshutdownscript.query", [["comment", "=", name]])
         if len(script_info) == 0:
             # No such script
             script_info = None
@@ -158,7 +163,7 @@ def main():
     if script_info is None:
         # Script doesn't exist
 
-        if state == 'present':
+        if state == "present":
             # Script is supposed to exist, so create it.
 
             # Collect arguments to pass to initshutdownscript.create()
@@ -168,47 +173,47 @@ def main():
 
             # Figure out what kind of script/command it is.
             if cmd is not None:
-                arg['type'] = "COMMAND"
-                arg['command'] = cmd
+                arg["type"] = "COMMAND"
+                arg["command"] = cmd
             elif path is not None:
-                arg['type'] = "SCRIPT"
-                arg['script'] = path
+                arg["type"] = "SCRIPT"
+                arg["script"] = path
             else:
-                arg['type'] = "SCRIPT"
-                arg['script_text'] = script
+                arg["type"] = "SCRIPT"
+                arg["script"] = script
 
             if when is not None:
-                arg['when'] = when.upper()
+                arg["when"] = when.upper()
 
             if timeout is not None:
-                arg['timeout'] = timeout
+                arg["timeout"] = timeout
 
             if module.check_mode:
-                result['msg'] = f"Would have created script {name} with {arg}"
+                result["msg"] = f"Would have created script {name} with {arg}"
             else:
                 #
                 # Create new script.
                 #
                 try:
                     err = mw.call("initshutdownscript.create", arg)
-                    result['msg'] = err
+                    result["msg"] = err
                 except Exception as e:
-                    result['failed_invocation'] = arg
+                    result["failed_invocation"] = arg
                     module.fail_json(msg=f"Error creating script {name}: {e}")
 
                 # Return whichever interesting bits initshutdownscript.create()
                 # returned.
-                result['script'] = err
+                result["script"] = err
 
-            result['changed'] = True
+            result["changed"] = True
         else:
             # Script is not supposed to exist.
             # All is well
-            result['changed'] = False
+            result["changed"] = False
 
     else:
         # Script exists
-        if state == 'present':
+        if state == "present":
             # Script is supposed to exist
 
             # Make list of differences between what is and what should
@@ -218,76 +223,72 @@ def main():
             # Check arguments, depending what kind of script this is.
             if cmd is not None:
                 # This is a command.
-                if script_info['type'] != "COMMAND":
-                    arg['type'] = "COMMAND"
-                if script_info['command'] != cmd:
-                    arg['cmd'] = cmd
-                if script_info['script'] != "":
-                    arg['script'] = ""
-                if script_info['script_text'] != "":
-                    arg['script_text'] = ""
+                if script_info["type"] != "COMMAND":
+                    arg["type"] = "COMMAND"
+                if script_info["command"] != cmd:
+                    arg["command"] = cmd
+                if script_info["script"] != "":
+                    arg["script"] = ""
             elif path is not None:
                 # This is a script, specified by pathname.
-                if script_info['type'] != "SCRIPT":
-                    arg['type'] = "SCRIPT"
-                if script_info['command'] != "":
-                    arg['cmd'] = ""
-                if script_info['script'] != path:
-                    arg['script'] = path
-                if script_info['script_text'] != "":
-                    arg['script_text'] = ""
+                if script_info["type"] != "SCRIPT":
+                    arg["type"] = "SCRIPT"
+                if script_info["command"] != "":
+                    arg["command"] = ""
+                if script_info["script"] != path:
+                    arg["script"] = path
             else:
                 # This is a script, specified by script body.
-                if script_info['type'] != "SCRIPT":
-                    arg['type'] = "SCRIPT"
-                if script_info['command'] != "":
-                    arg['cmd'] = ""
-                if script_info['script'] != "":
-                    arg['script'] = ""
-                if script_info['script_text'] != script:
-                    arg['script_text'] = script
+                if script_info["type"] != "SCRIPT":
+                    arg["type"] = "SCRIPT"
+                if script_info["command"] != "":
+                    arg["command"] = ""
+                if script_info["script"] != script:
+                    arg["script"] = script
 
-            if when is not None and script_info['when'] != when.upper():
-                arg['when'] = when.upper()
+            if when is not None and script_info["when"] != when.upper():
+                arg["when"] = when.upper()
 
-            if timeout is not None and script_info['timeout'] != timeout:
-                arg['timeout'] = timeout
+            if timeout is not None and script_info["timeout"] != timeout:
+                arg["timeout"] = timeout
 
             # If there are any changes, initshutdownscript.update()
             if len(arg) == 0:
                 # No changes
-                result['changed'] = False
+                result["changed"] = False
             else:
                 #
                 # Update script.
                 #
                 if module.check_mode:
-                    result['msg'] = f"Would have updated script {name}: {arg}"
+                    result["msg"] = f"Would have updated script {name}: {arg}"
                 else:
                     try:
-                        err = mw.call("initshutdownscript.update",
-                                      script_info['id'],
-                                      arg)
+                        err = mw.call(
+                            "initshutdownscript.update", script_info["id"], arg
+                        )
                     except Exception as e:
-                        module.fail_json(msg=f"Error updating script {name} with {arg}: {e}")
+                        module.fail_json(
+                            msg=f"Error updating script {name} with {arg}: {e}"
+                        )
                         # Return any interesting bits from err
-                        result['status'] = err['status']
-                result['changed'] = True
+                        result["status"] = err["status"]
+                result["changed"] = True
         else:
             # Script is not supposed to exist
 
             if module.check_mode:
-                result['msg'] = f"Would have deleted script {name}"
+                result["msg"] = f"Would have deleted script {name}"
             else:
                 try:
                     #
                     # Delete script.
                     #
-                    err = mw.call("initshutdownscript.delete",
-                                  script_info['id'])
+                    err = mw.call("initshutdownscript.delete", script_info["id"])
                 except Exception as e:
                     module.fail_json(msg=f"Error deleting script {name}: {e}")
-            result['changed'] = True
+            result["changed"] = True
+            result["changed"] = True
 
     module.exit_json(**result)
 
